6d5861a8f2d807879d6d75dfa33a96c5a8ec7951
Edwin Pedraza <edw.pedraza@gmail.com>
2023-01-04 13:24:11 +1000

    se realiza la seccion de testimonios y se inicia con contactos

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import './contact.css'
+import {MdEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {SiWhatsapp} from 'react-icons/si'
 
 const Contact = () => {
   return (
     <section id='contact'>
-      contact
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <article className='contact__option'>
+          <MdEmail/>
+          <h4>Email</h4>
+          <h5>Edwin@gmail.com</h5>
+          <a href="mailto:edw.pedraza@gmail.com" target= '_blank' rel='noreferrer'> Send a message</a>
+        </article>
+
+        <article className='contact__option'>
+          <RiMessengerLine/>
+          <h4>Messenger</h4>
+          <h5>Edwin@gmail.com</h5>
+          <a href="https://m.me/edwin.pedraza" target= '_blank' rel='noreferrer'> Send a message</a>
+        </article>
+
+        <article className='contact__option'>
+          <SiWhatsapp/>
+          <h4>WhatsApp</h4>
+          <h5>Edwin@gmail.com</h5>
+          <a href="https://wa.me/15551234567" target= '_blank' rel='noreferrer'> Send a message</a>
+        </article>
+
+
+      </div>
+
     </section>
   )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
 import './testimonials.css'
+import Avt1 from '../../assets/avatar1.jpg'
+import Avt2 from '../../assets/avatar2.jpg'
+import Avt3 from '../../assets/avatar3.jpg'
+import Avt4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import { Navigation, Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Testimonials = () => {
+
+  const data = [
+    {
+      avatar: Avt1,
+      name: 'Tina Snow',
+      review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, distinctio ipsum? Alias eum sequi assumenda optio asperiores et vitae dolor accusantium autem! Incidunt debitis sint, delectus facilis consectetur amet accusantium?'
+    },
+
+    {
+      avatar: Avt2,
+      name: 'Edwin Pedraza',
+      review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, distinctio ipsum? Alias eum sequi assumenda optio asperiores et vitae dolor accusantium autem! Incidunt debitis sint, delectus facilis consectetur amet accusantium?'
+    },
+
+    {
+      avatar: Avt3,
+      name: 'Orlando Gonzales',
+      review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, distinctio ipsum? Alias eum sequi assumenda optio asperiores et vitae dolor accusantium autem! Incidunt debitis sint, delectus facilis consectetur amet accusantium?'
+    },
+
+    {
+      avatar: Avt4,
+      name: 'Tomas Parket',
+      review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, distinctio ipsum? Alias eum sequi assumenda optio asperiores et vitae dolor accusantium autem! Incidunt debitis sint, delectus facilis consectetur amet accusantium?'
+    }
+  ]
+
+
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Reviw from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+      modules={[Navigation, Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      >
+        
+
+        {
+          data.map(({avatar,name,review}, index) =>{
+            return (
+              <SwiperSlide key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar} alt="Avatar one" />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>
+                  {review}
+                </small>
+              </SwiperSlide>
+            )
+          })
+        }
+
+      </Swiper>
+    </section>
   )
 }
 

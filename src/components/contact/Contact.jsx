982d7dc648294d76a22a70d12c0559a681d34ffa
Edwin Pedraza <edw.pedraza@gmail.com>
2023-01-15 16:57:22 +1000

    se realizan pruebas para publicar en github page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,29 +11,38 @@ const Contact = () => {
       <h2>Contact Me</h2>
 
       <div className="container contact__container">
-        <article className='contact__option'>
-          <MdEmail/>
-          <h4>Email</h4>
-          <h5>Edwin@gmail.com</h5>
-          <a href="mailto:edw.pedraza@gmail.com" target= '_blank' rel='noreferrer'> Send a message</a>
-        </article>
-
-        <article className='contact__option'>
-          <RiMessengerLine/>
-          <h4>Messenger</h4>
-          <h5>Edwin@gmail.com</h5>
-          <a href="https://m.me/edwin.pedraza" target= '_blank' rel='noreferrer'> Send a message</a>
-        </article>
-
-        <article className='contact__option'>
-          <SiWhatsapp/>
-          <h4>WhatsApp</h4>
-          <h5>Edwin@gmail.com</h5>
-          <a href="https://wa.me/15551234567" target= '_blank' rel='noreferrer'> Send a message</a>
-        </article>
+        <div className="contact__options">
 
 
-      </div>
+            <article className='contact__option'>
+              <MdEmail className='contact__option-icon'/>
+              <h4>Email</h4>
+              <h5>Edwin@gmail.com</h5>
+              <a href="mailto:edw.pedraza@gmail.com" target= '_blank' rel='noreferrer'> Send a message</a>
+            </article>
+
+            <article className='contact__option'>
+              <RiMessengerLine className='contact__option-icon'/>
+              <h4>Messenger</h4>
+              <h5>Edwin@gmail.com</h5>
+              <a href="https://m.me/edwin.pedraza" target= '_blank' rel='noreferrer'> Send a message</a>
+            </article>
+
+            <article className='contact__option'>
+              <SiWhatsapp className='contact__option-icon'/>
+              <h4>WhatsApp</h4>
+              <h5>Edwin@gmail.com</h5>
+              <a href="https://wa.me/15551234567" target= '_blank' rel='noreferrer'> Send a message</a>
+            </article>
+          </div>
+
+          <form action="">
+            <input type="text" name="name" placeholder='Your Full Name' required/>
+            <input type="email" name="email" placeholder='Your Email' required/>
+            <textarea name="messege" rows="7" placeholder='Message' required></textarea>
+            <button type='submit'className='btn btn-primary'>Send Message</button>
+          </form>
+        </div>
 
     </section>
   )
